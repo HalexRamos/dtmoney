e35a4ae8a2420dd50f0010154d47799feeb8bcb6
Halex Penha Ramos <halex@codebit.com.br>
2022-03-20 03:47:18 -0300

    Contextos e hooks

--- src/index.tsx
+++ src/index.tsx
@@ -11,24 +11,7 @@ createServer({
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelance de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2022-03-20 11:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2022-03-21 11:00:00'),
-        }
-      ],
+      transactions: [],
     })
   },
 
